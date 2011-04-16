952e39eb9899ab0ed8772fabbda586b607f01aed
Josh Long <josh.long@springsource.com>
2011-04-16 02:35:44 -0700

    initial commit of my old win32 code...

---
+++ jb2/jb-win32/src/main/java/com/joshlong/jukebox/system/integration/LinuxKernel26AudioCompactDisc.java
@@ -0,0 +1,10 @@
+package com.joshlong.jukebox.system.integration;
+
+/**
+ * implementation of the {@link AudioCompactDisc} interface. this works by using support in the Linux Kernel, 2.6 or higher.
+ *
+ * @author Josh Long
+ * @since 2.0
+ */
+public class LinuxKernel26AudioCompactDisc implements AudioCompactDisc {
+}
