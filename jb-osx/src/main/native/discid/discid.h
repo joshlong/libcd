ba05601ea065f51fd9760bc9742afd65514eb5e0
Josh Long <josh@joshlong.com>
2011-04-19 00:10:26 -0700

    incorporating skeletal code into native folder.

--- jb-osx/src/main/native/discid/discid.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISCID_H
-#define DISCID_H
-
-const unsigned FRAMES_PER_SECOND = 75;
-
-#endif // DISCID_H
---
+++ jb-osx/src/main/native/mac/MacIoCtlDisc.h
@@ -0,0 +1,23 @@
+#ifndef IOCTLDISC_H
+#define IOCTLDISC_H
+
+#include "disc.h"
+#include <IOKit/storage/IOCDTypes.h>
+
+class CMacIoCtlDisc : public CDisc
+{
+public:
+  CMacIoCtlDisc(const char *device = "/dev/rdisk1");
+  virtual ~CMacIoCtlDisc();
+
+  virtual unsigned GetTrackCount() const;
+  virtual unsigned long GetStartFrame(unsigned track) const;
+  virtual unsigned long GetFrames(unsigned track) const;
+
+private:
+  CDTOC *m_pToc;
+  unsigned int m_track_count;
+  static const unsigned int max_buffer = 1536;
+};
+
+#endif // IOCTLDISC_H
